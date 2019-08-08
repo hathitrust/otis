ff429cfd9a6f54450a69db44d5d6c73e0e1c42bc
Brian "Moses" Hall <moseshll@umich.edu>
2019-08-08 13:29:05 -0400

    Update ht_user_test.rb
    Add test 'iprestrict escaping and unescaping'

--- test/models/ht_user_test.rb
+++ test/models/ht_user_test.rb
@@ -1,7 +1,6 @@
 # frozen_string_literal: true
 
 require 'test_helper'
-require 'byebug'
 
 class HTUserTest < ActiveSupport::TestCase
   test 'iprestrict validation passes' do
@@ -16,5 +15,11 @@ class HTUserTest < ActiveSupport::TestCase
 
   test 'iprestrict to English' do
     assert_equal HTUser.human_attribute_name(:iprestrict), 'IP Restriction'
+  end
+  
+  test 'iprestrict escaping and unescaping' do
+    user = HTUser.new(userid: 'user', iprestrict: '127.0.0.1')
+    assert_equal user[:iprestrict], '^127\.0\.0\.1$'
+    assert_equal user.iprestrict, '127.0.0.1'
   end
 end
