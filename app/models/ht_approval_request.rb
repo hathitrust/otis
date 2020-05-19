d3a45777a17b1167b44e1861ce12b8959022fcc7
Aaron Elkiss <aelkiss@umich.edu>
2020-05-19 15:31:10 -0400

    HT-2382: don't assume secret_key_base is in secrets
    Using Rails.application.secret_key_base allows using either secrets or
    encrypted credential sotrage

--- app/models/ht_approval_request.rb
+++ app/models/ht_approval_request.rb
@@ -88,7 +88,7 @@ class HTApprovalRequest < ApplicationRecord
     text = text.to_s unless text.is_a? String
     len = ActiveSupport::MessageEncryptor.key_len
     salt = SecureRandom.hex len
-    key = ActiveSupport::KeyGenerator.new(Rails.application.secrets.secret_key_base).generate_key salt, len
+    key = ActiveSupport::KeyGenerator.new(Rails.application.secret_key_base).generate_key salt, len
     crypt = ActiveSupport::MessageEncryptor.new key
     encrypted_data = crypt.encrypt_and_sign text
     "#{salt}$$#{encrypted_data}"
@@ -99,7 +99,7 @@ class HTApprovalRequest < ApplicationRecord
 
     salt, data = text.split '$$'
     len = ActiveSupport::MessageEncryptor.key_len
-    key = ActiveSupport::KeyGenerator.new(Rails.application.secrets.secret_key_base).generate_key salt, len
+    key = ActiveSupport::KeyGenerator.new(Rails.application.secret_key_base).generate_key salt, len
     crypt = ActiveSupport::MessageEncryptor.new key
     crypt.decrypt_and_verify data
   end
