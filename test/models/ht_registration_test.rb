ac786ee475d1b00740ff831fa1373f4b81afcde5
mw <mwarin@umich.edu>
2021-10-26 12:18:37 -0500

    removed dumb test

--- test/models/ht_registration_test.rb
+++ test/models/ht_registration_test.rb
@@ -3,10 +3,6 @@
 require "test_helper"
 
 class HTRegistrationTest < ActiveSupport::TestCase
-  test "basic math" do
-    assert_equal(1, 1)
-  end
-
   test "validation passes" do
     assert build(:ht_registration).valid?
   end
