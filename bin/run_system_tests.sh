d24ac40197932834fcc9ba647a8047238592b8c5
moseshll <moseshll@umich.edu>
2022-10-07 13:45:14 -0400

    DEV-448 OTIS system tests fail silently - Bail out of `bin/run_system_tests.sh` with nonzero exit status instead of running cleanup code. - Fix approval request URL extraction in system tests when there are multiple URLs.

--- bin/run_system_tests.sh
+++ bin/run_system_tests.sh
@@ -6,4 +6,5 @@ bin/wait-for mariadb-test:3306
 bin/wait-for chrome-server:4444
 bundle exec rake db:seed
 bundle exec rake test:system
+[ $? -eq 0 ] || exit $?
 bundle exec rake otis:clear_database RAILS_ENV=test
