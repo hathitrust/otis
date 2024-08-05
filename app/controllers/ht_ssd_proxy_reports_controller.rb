0424450ce349bf7be488471a6860f3d506a42daa
Brian Moses Hall <moseshll@umich.edu>
2024-08-05 16:15:16 -0400

    Enable date range filtering and data export.

--- app/controllers/ht_ssd_proxy_reports_controller.rb
+++ app/controllers/ht_ssd_proxy_reports_controller.rb
@@ -4,6 +4,8 @@ class HTSSDProxyReportsController < ApplicationController
   def index
     reports = HTSSDProxyReport.order(:datetime)
     @reports = reports.map { |r| presenter r }
+    @time_start = HTSSDProxyReport.minimum(:datetime).to_date.to_s
+    @time_end = HTSSDProxyReport.maximum(:datetime).to_date.to_s
   end
 
   private
