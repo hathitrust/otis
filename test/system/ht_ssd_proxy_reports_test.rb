f9307c89ad4b467cbaf10e76709fb811496f173e
Brian Moses Hall <moseshll@umich.edu>
2024-11-18 17:20:00 -0500

    Fix system tests looking for an h1 instead of an h2

--- test/system/ht_ssd_proxy_reports_test.rb
+++ test/system/ht_ssd_proxy_reports_test.rb
@@ -3,6 +3,6 @@ require "application_system_test_case"
 class HTSSDProxyReportsTest < ApplicationSystemTestCase
   test "visit index" do
     visit_with_login ht_ssd_proxy_reports_url
-    assert_selector "h1", text: "SSD Proxy Reports"
+    assert_selector "h2", text: "SSD Proxy Reports"
   end
 end
--- test/system/ht_users_test.rb
+++ test/system/ht_users_test.rb
@@ -3,6 +3,6 @@ require "application_system_test_case"
 class HTUsersTest < ApplicationSystemTestCase
   test "visit index" do
     visit_with_login ht_users_url
-    assert_selector "h1", text: "Users"
+    assert_selector "h2", text: "Users"
   end
 end
