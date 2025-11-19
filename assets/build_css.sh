138c31c9fdb7aeb684123bd171364c4b8b1850f6
Brian Moses Hall <moseshll@umich.edu>
2025-11-19 15:38:37 -0500

    Fumbling around with the elephant SVG on landing pages

--- assets/build_css.sh
+++ assets/build_css.sh
@@ -1,4 +1,4 @@
 cp node_modules/bootstrap-icons/font/fonts/* public/styles/fonts &&
 cp node_modules/firebird-common/src/public/fonts/mulish* public/fonts &&
-cp node_modules/firebird-common/src/public/bg404desktop.svg public/images &&
+cp node_modules/firebird-common/src/public/elephant.svg public/images &&
 sass $* assets/styles:public/styles --style=compressed --load-path=node_modules
