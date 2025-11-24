23eeb559a634144bbda27eda13ede5e460255343
Brian "Moses" Hall <moseshll@umich.edu>
2025-11-24 14:43:24 -0500

    ETT-294 Implement usability and branding improvements to OTIS elevate… (#274)
    * ETT-294 Implement usability and branding improvements to OTIS elevated access end user facing screens
      - Add firebird-common and integrate into CSS pipeline
      - Reword registration landing pages with new text
      - Re-style registration landing pages with more obviously HT branding
    * Ease of use transition from email field to menu in fake login form
    * [a11y] Fix text contrast issue with bg-warning badge in user presenter
    * Fix bugs in ht_user expiration date widgets not syncing "expiring soon" appearance with current state.
    
    Co-authored-by: Caryl Wyatt <carylw@hathitrust.org>

--- test/controllers/fake_shib_controller_test.rb
+++ test/controllers/fake_shib_controller_test.rb
@@ -6,7 +6,7 @@ class FakeShibControllerTest < ActionDispatch::IntegrationTest
   test "should get login form" do
     get fake_shib_url
     assert_equal "new", @controller.action_name
-    assert_select "input[name=username]"
+    assert_select "select[name=username]"
     assert_select "input[type=submit][name=login]"
   end
 end
