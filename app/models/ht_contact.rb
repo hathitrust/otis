e81e22357fe64dd59076521eea1d742c40e8bf22
Brian "Moses" Hall <moseshll@umich.edu>
2026-09-01 12:02:39 -0400

    Call `HTContactType.initialize_builtin_types!` before rspec suite.

--- app/models/ht_contact.rb
+++ app/models/ht_contact.rb
@@ -19,7 +19,7 @@ class HTContact < ApplicationRecord
   # Add or update based on email and contact type id.
   # Can be triggered by adding or editing a registration, or editing a user.
   # Use `tap` instead of passing a block to `find_or_initialize_by` because we want
-  # to update name and institution even if record already exists. (Although they are
+  # to update name and institution even if record already exists. Although they are
   # unlikely to change substantially, new registration data should probably override
   # old.
   def self.add_or_update(contact_type:, email:, inst_id:, name:)
