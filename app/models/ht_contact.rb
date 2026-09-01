16ab02fa6fcf4800606bac8de7a0296a489dd904
Brian "Moses" Hall <moseshll@umich.edu>
2026-09-01 11:47:39 -0400

    Add `HTContact.add_or_update` class method for synchronizing to registration and user data.

--- app/models/ht_contact.rb
+++ app/models/ht_contact.rb
@@ -15,4 +15,18 @@ class HTContact < ApplicationRecord
   validates :contact_type, presence: true
   validates :email, presence: true, format: {with: URI::MailTo::EMAIL_REGEXP}
   validates :name, presence: true
+
+  # Add or update based on email and contact type id.
+  # Can be triggered by adding or editing a registration, or editing a user.
+  # Use `tap` instead of passing a block to `find_or_initialize_by` because we want
+  # to update name and institution even if record already exists. (Although they are
+  # unlikely to change substantially, new registration data should probably override
+  # old.
+  def self.add_or_update(contact_type:, email:, inst_id:, name:)
+    find_or_initialize_by(email: email, contact_type: contact_type).tap do |approver|
+      approver.inst_id = inst_id
+      approver.name = name
+      approver.save!
+    end
+  end
 end
--- spec/models/ht_contact_spec.rb
+++ spec/models/ht_contact_spec.rb
@@ -1,7 +1,16 @@
 # frozen_string_literal: true
 
 RSpec.describe HTContact do
-  let(:factory) { :ht_contact_type }
+  let(:factory) { :ht_contact }
+  let(:test_email) { "contact@default.invalid" }
+  let(:test_name) { "Contact Name" }
+  let(:test_type) { HTContactType.ea_approver.id }
+  let(:test_inst_id) { create(:ht_institution).id }
+
+  around(:each) do |example|
+    described_class.delete_all
+    example.run
+  end
 
   describe "validations" do
     describe "name" do
@@ -9,6 +18,75 @@ RSpec.describe HTContact do
         expect(build(factory, name: nil).valid?).to be false
         expect(build(factory, name: "").valid?).to be false
         expect(build(factory, name: "Test Name").valid?).to be true
+      end
+    end
+  end
+
+  describe ".add_or_update" do
+    context "with no matching email + type" do
+      it "creates a mew contact" do
+        expect {
+          described_class.add_or_update(
+            contact_type: test_type,
+            email: test_email,
+            inst_id: test_inst_id,
+            name: test_name
+          )
+        }.to change { described_class.count }.by(1)
+      end
+
+      it "populates all attributes" do
+        expect(described_class.where(contact_type: test_type, email: test_email).count).to eq 0
+        approver = described_class.add_or_update(
+          contact_type: test_type,
+          email: test_email,
+          inst_id: test_inst_id,
+          name: test_name
+        )
+        expect(approver.contact_type).to eq test_type
+        expect(approver.email).to eq test_email
+        expect(approver.inst_id).to eq test_inst_id
+        expect(approver.name).to eq test_name
+      end
+    end
+
+    context "with existing record" do
+      it "does not create a mew contact" do
+        create(
+          factory,
+          contact_type: test_type,
+          email: test_email,
+          inst_id: create(:ht_institution).id,
+          name: "Existing Name"
+        )
+        expect {
+          described_class.add_or_update(
+            contact_type: test_type,
+            email: test_email,
+            inst_id: test_inst_id,
+            name: test_name
+          )
+        }.to change { described_class.count }.by(0)
+      end
+
+      it "updates all attributes" do
+        create(
+          factory,
+          contact_type: test_type,
+          email: test_email,
+          inst_id: create(:ht_institution).id,
+          name: "Existing Name"
+        )
+        approver = described_class.add_or_update(
+          contact_type: test_type,
+          email: test_email,
+          inst_id: test_inst_id,
+          name: test_name
+        )
+        expect(approver.contact_type).to eq test_type
+        expect(approver.email).to eq test_email
+        expect(approver.inst_id).to eq test_inst_id
+        expect(approver.name).to eq test_name
       end
     end
   end
